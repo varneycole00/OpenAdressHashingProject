e080140f9cd7321848eae1456923ea41fdcbf9cf
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2020-05-11 21:44:50 -0400

    Implementation testing the time it takes to search for values

--- src/Main.java
+++ src/Main.java
@@ -10,60 +10,40 @@ public class Main {
 
 
         // testing for LinearProbingHashSymbol tables of different sizes(MF * number of words in the dictionary)
-        for(int i = 8; i > 1; i = i/2) {
-            LinearProbingHashST<Integer, String> table = new LinearProbingHashST<>(i*321165);
-            Scanner sc = new Scanner(new File("words.english.txt"));
-            while(sc.hasNext()) {
-                String s = sc.next();
-                table.put(s.length(), s);
-            }
-            System.out.println("The hash table with mf " + i + " has been filled");
-            Scanner checker = new Scanner(new File("words.english.txt"));
-            Stopwatch timer = new Stopwatch();
-            while(checker.hasNext()) {
-                String next = checker.next();
-                String keyValues = table.get(next.length());
-                if(!next.equals(table.get(next.length()))) {
-                    System.out.println("The next item is not in the right location in the table");
+        System.out.println("All words in the dictionary performance: ");
+
+        // testing for LinearProbingHashSymbol tables of different sizes(MF * number of words in the dictionary)
+        for(int j = 321165; j > 40144; j = j/2) {
+            System.out.println("Performance of table including " + j + " words:");
+            for (double i = 8; i > .6; i = i / 2) {
+                if (i == .625) {
+                    i = 1.125;
                 }
-            }
-            double time = timer.elapsedTime();
-            System.out.println("Time to complete table with mf " + i + " is: " + time);
-        }
-
-
-        // creation of the table that will hold every word
-        LinearProbingHashST<Integer, String> table = new LinearProbingHashST<>(8*321165);
-
-        // Scanner to take in the dicionary
-        Scanner sc = new Scanner(new File("words.english.txt"));
-
-        // adding each word to the table
-        int key = 0;
-        while(sc.hasNext()) {
-            String s = sc.next();
-            table.put(key, s);
-            key++;
-        }
-        System.out.println("The LinearProbingHashST has been filled with " +
-                "the english dictionary");
-        System.out.println("The size of the table: " + table.size());
-        Scanner sc2 = new Scanner(new File("words.english.txt"));
-        key = 0;
-
-        Stopwatch timer = new Stopwatch();
-
-        while(sc2.hasNext()) {
-            String s = sc2.next();
-            if(table.contains(key)) {
-                String word = table.get(key);
-                if(!s.equals(word)) {
-                    System.out.println("Did not return correct word");
+                if (i == .75) {
+                    i = 1.25;
                 }
+                if (i == 1) {
+                    i = 1.5;
+                }
+                LinearProbingHashST<String, String> table = new LinearProbingHashST<>((int) i * 321165);
+                Scanner sc = new Scanner(new File("words.english.txt"));
+                while (sc.hasNext()) {
+                    String s = sc.next();
+                    table.put(s, s);
+                }
+                Scanner checker = new Scanner(new File("words.english.txt"));
+                Stopwatch timer = new Stopwatch();
+                int checked = 0;
+                while (checked < j) {
+                    String next = checker.next();
+                    String keyValues = table.get(next);
+                    checked++;
+                }
+                double time = timer.elapsedTime();
+                System.out.println("\t Time to complete table search with mf " + i + " is: " + time);
             }
-            key++;
         }
-        double time = timer.elapsedTime();
-        System.out.println("The amount of time it took to find each object in the table: " + time);
+
+
     }
 }
