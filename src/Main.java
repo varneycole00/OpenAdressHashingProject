9b7a998787c1df0431a19003b3e0eab3891d2672
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2020-05-11 22:31:44 -0400

    creation and search performance analytics in main

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-
-        // testing for LinearProbingHashSymbol tables of different sizes(MF * number of words in the dictionary)
-        System.out.println("All words in the dictionary performance: ");
-
-        // testing for LinearProbingHashSymbol tables of different sizes(MF * number of words in the dictionary)
+        System.out.print("Table Creation Performance: ");
+        System.out.println();
         for(int j = 321165; j > 40144; j = j/2) {
-            System.out.println("Performance of table including " + j + " words:");
-            for (double i = 8; i > .6; i = i / 2) {
+            System.out.println("Creation of tables size " + j + ": ");
+            for(double i = 8; i > .5; i = i/2) {
                 if (i == .625) {
                     i = 1.125;
                 }
@@ -25,7 +22,43 @@ public class Main {
                 if (i == 1) {
                     i = 1.5;
                 }
-                LinearProbingHashST<String, String> table = new LinearProbingHashST<>((int) i * 321165);
+                if(i == .5625) {
+                    i = .75;
+                }
+                LinearProbingHashST<String, String> table = new LinearProbingHashST<>((int) (i * j));
+                Scanner sc = new Scanner(new File("words.english.txt"));
+                int progress = 0;
+                Stopwatch timer = new Stopwatch();
+                while(progress < j) {
+                    String next = sc.next();
+                    table.put(next,next);
+                    progress++;
+                }
+                double time = timer.elapsedTime();
+                System.out.println("\t Time to complete table creation with mf " + i + " is: \t" + time);
+            }
+        }
+
+        System.out.println("Table Searching Performance");
+        System.out.println();
+        // testing for LinearProbingHashSymbol tables of different sizes(MF * number of words in the dictionary)
+        for(int j = 321165; j > 40144; j = j/2) {
+            System.out.println("Performance of table including " + j + " words:");
+            for (double i = 8; i > .5; i = i / 2) {
+
+                if (i == .625) {
+                    i = 1.125;
+                }
+                if (i == .75) {
+                    i = 1.25;
+                }
+                if (i == 1) {
+                    i = 1.5;
+                }
+                if(i == .5625) {
+                    i = .75;
+                }
+                LinearProbingHashST<String, String> table = new LinearProbingHashST<>((int) (i * j));
                 Scanner sc = new Scanner(new File("words.english.txt"));
                 while (sc.hasNext()) {
                     String s = sc.next();
@@ -40,10 +73,40 @@ public class Main {
                     checked++;
                 }
                 double time = timer.elapsedTime();
-                System.out.println("\t Time to complete table search with mf " + i + " is: " + time);
+                System.out.println("\t Time to complete table search with mf " + i + " is:   \t" + time);
             }
         }
 
+        System.out.println("Full Table Deletion Performance: ");
+        System.out.println();
+        for(int j = 321165; j > 40144; j = j/2) {
+            System.out.println("Creation of tables size " + j + ": ");
+            for(double i = 8; i > .5; i = i/2) {
+                if (i == .625) {
+                    i = 1.125;
+                }
+                if (i == .75) {
+                    i = 1.25;
+                }
+                if (i == 1) {
+                    i = 1.5;
+                }
+                if(i == .5625) {
+                    i = .75;
+                }
+                LinearProbingHashST<String, String> table = new LinearProbingHashST<>((int) (i * j));
+                Scanner sc = new Scanner(new File("words.english.txt"));
+                int progress = 0;
+                Stopwatch timer = new Stopwatch();
+                while(progress < j) {
+                    String next = sc.next();
+                    table.put(next,next);
+                    progress++;
+                }
+                double time = timer.elapsedTime();
+                System.out.println("\t Time to complete table creation with mf " + i + " is: \t" + time);
+            }
+        }
 
     }
 }
